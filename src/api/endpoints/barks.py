119622088c246add81864eb054bec98a7f2e2454
jusmccar <justinmccartney1@yahoo.com>
2025-12-02 18:19:59 -0800

    Add bark 3

--- src/api/endpoints/barks.py
+++ src/api/endpoints/barks.py
@@ -8,4 +8,4 @@ def barks_list(request):
     """
     Bark list endpoint that returns a list of barks.
     """
-    return [{"id": 1, "message": "bark 1!"}, {"id": 2, "message": "bark 2!"}]
+    return [{"id": 1, "message": "bark 1!"}, {"id": 2, "message": "bark 2!"}, {"id":3, "message": "bark 3!"}]
