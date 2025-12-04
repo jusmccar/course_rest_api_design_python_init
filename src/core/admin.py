ecfdef55e0840575c98e1ed73447c80fb3c03f23
jusmccar <justinmccartney1@yahoo.com>
2025-12-03 20:27:48 -0800

    Add BarkAdmin admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
+from core.models import BarkModel
 from core.models import DogUserModel
 
 @admin.register(DogUserModel)
 class DogUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BarkModel)
+class BarkAdmin(admin.ModelAdmin):
     pass
