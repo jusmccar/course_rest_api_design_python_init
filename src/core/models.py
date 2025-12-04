3a07a4b7ff23385ddc19c4e04efd6ff9b80b7622
jusmccar <justinmccartney1@yahoo.com>
2025-12-03 20:22:44 -0800

    Add BarkModel model

--- src/core/models.py
+++ src/core/models.py
@@ -23,3 +23,21 @@ class DogUserModel(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+class BarkModel(BaseModel):
+    """Custom bark model for barks."""
+
+    message = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        DogUserModel,
+        on_delete=models.CASCADE,
+        related_name="barks",
+    )
+
+    class Meta:
+        verbose_name = "Bark"
+        verbose_name_plural = "Barks"
+
+    def __str__(self):
+        return f"{self.user.username} - {self.message[:20]}..."
