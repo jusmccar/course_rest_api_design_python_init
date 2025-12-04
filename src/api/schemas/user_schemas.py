96da2adef5be6641714b314ef06f7e45b82bf717
jusmccar <justinmccartney1@yahoo.com>
2025-12-04 03:36:18 -0800

    Add field validator to username field in DogUserCreateSchemaIn

--- src/api/schemas/user_schemas.py
+++ src/api/schemas/user_schemas.py
@@ -1,12 +1,24 @@
 from ninja import ModelSchema
+from pydantic import field_validator
+
 from core.models import DogUserModel
 
 class DogUserCreateSchemaIn(ModelSchema):
     """Schema for dog user requests"""
+    username: str
 
     class Meta:
         model = DogUserModel
         fields = ["username"]
+
+    @field_validator('username')
+    @classmethod
+    def validate_username_length(cls, v: str) -> str:
+        """Ensure username is at least 3 characters long"""
+        if len(v) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+
+        return v
 
 class DogUserSchemaOut(ModelSchema):
     """Schema for dog user responses"""
