a00f47d0e78bdbfe910803130897605e70d72bbc
jusmccar <justinmccartney1@yahoo.com>
2025-12-05 10:12:15 -0800

    Add AuthTokenModel model

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,10 @@
+import binascii
+import os
+from uuid import uuid4
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from uuid import uuid4
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
@@ -42,3 +46,39 @@ class BarkModel(BaseModel):
 
     def __str__(self):
         return f"{self.user.username} - {self.message[:20]}..."
+
+class AuthTokenModel(BaseModel):
+    """Represents an authentication token for a user"""
+
+    key = models.CharField(max_length=40, unique=True)
+    user = models.ForeignKey(
+        to=DogUserModel, related_name="auth_tokens", on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    expires = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Auth Token"
+        verbose_name_plural = "Auth Tokens"
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+
+        return super().save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def is_expired(self):
+        if self.expires is None:
+            return False
+
+        return timezone.now() >= self.expires
+
+    def is_valid(self):
+        return self.is_active and not self.is_expired()
+
+    def __str__(self):
+        return f"Token {self.key[:6]}... for {self.user.username}"
