62b4582ef68425f9445669085d02fad19cc1f514
jusmccar <justinmccartney1@yahoo.com>
2025-12-07 00:35:12 -0800

    Add get_jwt_token function

--- src/api/endpoints/auth.py
+++ src/api/endpoints/auth.py
@@ -7,6 +7,7 @@ from api.schemas.auth_schemas import RefreshTokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaOut
 from api.schemas.common_schemas import ErrorSchemaOut
+from common.auth.jwt_auth import create_jwt
 from core.models import AuthTokenModel
 
 router = Router()
@@ -48,3 +49,19 @@ def refresh_token(request, credentials: RefreshTokenRequestSchemaIn):
     expires_in = int((access_token.expires - timezone.now()).total_seconds())
 
     return (200, {"access_token": access_token.key, "refresh_token": refresh_token.key, "expires_in": expires_in})
+
+@router.post("/jwt-token/", response={200: TokenRequestSchemaOut, 401: ErrorSchemaOut}, auth=None)
+def get_jwt_token(request, credentials: TokenRequestSchemaIn):
+    """
+    JWT token endpoint that returns a JWT token.
+    """
+    user = authenticate(username=credentials.username, password=credentials.password)
+
+    if not user:
+        return (401, {"error": "Invalid credentials"})
+
+    access_token = create_jwt(user_id=user.id, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
+    refresh_token = create_jwt(user_id=user.id, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
+    expires_in = int(timedelta(hours=4).total_seconds())
+
+    return (200, {"access_token": access_token, "refresh_token": refresh_token, "expires_in": expires_in})
