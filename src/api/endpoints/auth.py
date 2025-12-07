435d3d5e5c375684ace0de095abfbe196e90cf36
jusmccar <justinmccartney1@yahoo.com>
2025-12-06 23:48:21 -0800

    Add refresh_token function

--- src/api/endpoints/auth.py
+++ src/api/endpoints/auth.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth import authenticate
 from ninja import Router
 
+from api.schemas.auth_schemas import RefreshTokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaOut
 from api.schemas.common_schemas import ErrorSchemaOut
@@ -24,6 +25,26 @@ def get_token(request, credentials: TokenRequestSchemaIn):
     AuthTokenModel.objects.filter(user=user).delete()
     access_token = AuthTokenModel.objects.create(user=user, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
     refresh_token = AuthTokenModel.objects.create(user=user, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
+    expires_in = int((access_token.expires - timezone.now()).total_seconds())
+
+    return (200, {"access_token": access_token.key, "refresh_token": refresh_token.key, "expires_in": expires_in})
+
+@router.post("/token/refresh/", response={200: TokenRequestSchemaOut, 401: ErrorSchemaOut}, auth=None)
+def refresh_token(request, credentials: RefreshTokenRequestSchemaIn):
+    """
+    Token endpoint that refreshes a token.
+    """
+    obj = AuthTokenModel.objects.filter(key=credentials.refresh_token, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH, is_active=True).first()
+
+    if not obj:
+        return (401, {"error": "Invalid refresh token"})
+
+    if obj.is_expired():
+        return (401, {"error": "Expired refresh token"})
+
+    AuthTokenModel.objects.filter(user=obj.user).delete()
+    access_token = AuthTokenModel.objects.create(user=obj.user, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
+    refresh_token = AuthTokenModel.objects.create(user=obj.user, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
     expires_in = int((access_token.expires - timezone.now()).total_seconds())
 
     return (200, {"access_token": access_token.key, "refresh_token": refresh_token.key, "expires_in": expires_in})
