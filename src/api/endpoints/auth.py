03b1a31d14015e6d1914d84ae0dc1fe4df439093
jusmccar <justinmccartney1@yahoo.com>
2025-12-10 02:00:03 -0800

    Add handle_refresh_jwt_token function

--- src/api/endpoints/auth.py
+++ src/api/endpoints/auth.py
@@ -1,22 +1,15 @@
-from datetime import timedelta
-import jwt
-
-from django.conf import settings
-from django.contrib.auth import authenticate
-from django.utils import timezone
 from ninja import Router
 
 from api.logic.auth_logic import handle_get_token
 from api.logic.auth_logic import handle_get_jwt_token
 from api.logic.auth_logic import handle_refresh_token
+from api.logic.auth_logic import handle_refresh_jwt_token
 from api.logic.exceptions import get_error_response
 from api.schemas.auth_schemas import RefreshTokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaIn
 from api.schemas.auth_schemas import TokenRequestSchemaOut
 from api.schemas.common_schemas import ErrorSchemaOut
-from common.auth.jwt_auth import create_jwt
 from core.models import AuthTokenModel
-from core.models import DogUserModel
 
 router = Router()
 
@@ -79,24 +72,13 @@ def refresh_jwt_token(request, credentials: RefreshTokenRequestSchemaIn):
     """
     JWT token endpoint that refreshes a JWT token.
     """
+    refresh_token = credentials.refresh_token
+
     try:
-        payload = jwt.decode(credentials.refresh_token, settings.JWT_SECRET, algorithms=["HS256"])
-    except jwt.DecodeError:
-        return (401, {"error": "Invalid refresh token"})
-    except jwt.ExpiredSignatureError:
-        return (401, {"error": "Expired refresh token"})
+        data = handle_refresh_jwt_token(refresh_token)
+    except Exception as e:
+        status_code, error_response = get_error_response(e)
 
-    if payload.get("token_type") != "refresh":
-        return (401, {"error": "Invalid refresh token"})
+        return (status_code, error_response)
 
-    user_id = payload.get("user_id")
-    obj = DogUserModel.objects.filter(id=user_id).first()
-
-    if not obj:
-        return (401, {"error": "Invalid refresh token"})
-
-    access_token = create_jwt(user_id=user_id, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
-    refresh_token = create_jwt(user_id=user_id, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
-    expires_in = int(timedelta(hours=4).total_seconds())
-
-    return (200, {"access_token": access_token, "refresh_token": refresh_token, "expires_in": expires_in})
+    return (200, data)
--- src/api/logic/auth_logic.py
+++ src/api/logic/auth_logic.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
+import jwt
 
+from django.conf import settings
 from django.contrib.auth import authenticate
 from django.utils import timezone
 
@@ -8,6 +10,7 @@ from api.logic.exceptions import TokenExpiredError
 from api.logic.exceptions import TokenInvalidError
 from common.auth.jwt_auth import create_jwt
 from core.models import AuthTokenModel
+from core.models import DogUserModel
 
 
 def handle_get_token(username: str, password: str) -> dict:
@@ -49,6 +52,30 @@ def handle_get_jwt_token(username: str, password: str) -> dict:
 
     access_token = create_jwt(user_id=user.id, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
     refresh_token = create_jwt(user_id=user.id, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
+    expires_in = int(timedelta(hours=4).total_seconds())
+
+    return {"access_token": access_token, "refresh_token": refresh_token, "expires_in": expires_in}
+
+
+def handle_refresh_jwt_token(refresh_token: str) -> dict:
+    try:
+        data = jwt.decode(refresh_token, settings.JWT_SECRET, algorithms=["HS256"])
+    except jwt.DecodeError:
+        raise TokenInvalidError("Invalid refresh token")
+    except jwt.ExpiredSignatureError:
+        raise TokenExpiredError("Expired refresh token")
+
+    if data.get("token_type") != "refresh":
+        raise TokenInvalidError("Invalid refresh token")
+
+    user_id = data.get("user_id")
+    user_obj = DogUserModel.objects.filter(id=user_id).first()
+
+    if not user_obj:
+        raise TokenInvalidError("Invalid refresh token")
+
+    access_token = create_jwt(user_id=user_id, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
+    refresh_token = create_jwt(user_id=user_id, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
     expires_in = int(timedelta(hours=4).total_seconds())
 
     return {"access_token": access_token, "refresh_token": refresh_token, "expires_in": expires_in}
